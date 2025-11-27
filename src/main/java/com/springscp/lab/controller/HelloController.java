33a3fe45af2fdbc7536747041dd391a42521ea62
peter <peter.bodi@yahoo.com>
2025-11-27 22:06:28 +0100

    added hello controller

---
+++ src/main/java/com/springscp/lab/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.springscp.lab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Spring SCP is ready!";
+    }
+}
