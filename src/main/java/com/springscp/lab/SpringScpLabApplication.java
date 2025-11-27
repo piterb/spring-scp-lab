e658a2461b61f6baffd17bc28c7c3092fcdb64a9
Peter <peter@Peters-MacBook-Air.local>
2025-11-27 21:52:47 +0100

    Initial commit

---
+++ src/main/java/com/springscp/lab/SpringScpLabApplication.java
@@ -0,0 +1,13 @@
+package com.springscp.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScpLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScpLabApplication.class, args);
+	}
+
+}
